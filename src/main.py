233467c2b98583ac953bbaf3a689c629ad0a30d2
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2025-05-26 01:30:39 +0600

    feat: implemented split_nodes_delimiter() function
    In this function we converted inline markdown to list of TextNode.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from inline_markdown import split_nodes_delimiter
 
 
 def main():
-    # test text_node_to_html_node function
-    node = TextNode("Click here", TextType.LINK, "https://example.com")
-    html_node = text_node_to_html_node(node)
-    node2 = TextNode("An image of a cat", TextType.IMAGE, "https://example.com/cat.jpg")
-    html_node2 = text_node_to_html_node(node2)
-    print(html_node.value, html_node2.value)
-    print(html_node.tag, html_node2.tag)
-    print(html_node.props, html_node2.props)
-    print(html_node.children, html_node2.children)
+    # test inline_markdown delimiter
+    node = TextNode("**bold** and _italic_", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+    print(new_nodes)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    print(new_nodes)
 
 main()
