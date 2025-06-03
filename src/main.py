e850a59de3be1d635e2238b944034c991c87761a
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2025-06-03 14:51:57 +0600

    feat: Implemented markdown_to_html_node function
    - Implemented markdown_to_html_node function
    - Used single responsibility principle
    - Added testcases for the markdown_to_html_node function

--- src/main.py
+++ src/main.py
@@ -11,13 +11,38 @@ from inline_markdown import (
 from markdown_blocks import (
     markdown_to_blocks,
     block_to_block_type,
+    markdown_to_html_node,
 )
 
 
 def main():
     # test test_markdown_to_blocks function
-    md = "1. First item\n2. Second item\n3. Third item"
-    block_type = block_to_block_type(md)
-    print(block_type)
+    # md = "## This is a **bold** heading with _italic_ text"
+#     md = """
+# This is **bolded** paragraph
+# text in a p
+# tag here
+#
+# This is another paragraph with _italic_ text and `code` here
+#
+# """
+#     md = """
+# ```
+# This is text that _should_ remain
+# the **same** even with inline stuff
+# ```
+# """
+    md = """
+> This is a quote with **bold text** and _italic text_
+> It can also have `code` and even links
+> Multiple lines are all part of the same quote
+"""
+#     md = """
+# 1. First item with **bold text**
+# 2. Second item with _italic text_
+# 3. Third item with `code` and more text
+# """
+    blocks = markdown_to_html_node(md)
+    print(blocks.to_html())
 
 main()
